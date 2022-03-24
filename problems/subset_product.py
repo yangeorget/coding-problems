ec564cb099e6410eff23a75a40d73f05a0d0ff2a
Yan Georget <yan@botfuel.io>
2022-03-24 12:33:05 +0100

    Improve subset product algo

--- problems/subset_product.py
+++ problems/subset_product.py
@@ -7,25 +7,21 @@ class SubsetProduct:
     See https://practice.geeksforgeeks.org/problems/number-of-subsets-with-product-less-than-k/0/
     """
 
+    def products(self, arr, k):
+        products = [1]
+        for x in arr:
+            products.extend([x * y for y in products if x * y <= k])
+        return sorted(products[1:])
+
     def count(self, arr, n, k):
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1 if arr[0] <= k else 0
-        N2 = n // 2
-        arr1 = [a for i, a in enumerate(arr) if i < N2 and a <= k]
-        arr2 = [a for i, a in enumerate(arr) if i >= N2 and a <= k]
-        count = self.count(arr1, len(arr1), k) + self.count(arr2, len(arr2), k)
-        prod1 = [1]
-        while len(arr1) > 0:
-            a1 = arr1.pop()
-            prod1 = [*prod1, *[a1 * a for a in prod1 if a1 * a <= k]]
-        prod1 = sorted(prod1[1:])
-        prod2 = [1]
-        while len(arr2) > 0:
-            a2 = arr2.pop()
-            prod2 = [*prod2, *[a2 * a for a in prod2 if a2 * a <= k]]
-        prod2 = sorted(prod2[1:])
-        for i in range(0, len(prod1)):
-            count += bisect(prod2, (k // prod1[i]))  # TODO: fix
+        arr = [a for a in arr if a <= k]
+        if n <= 1:
+            return n
+        left = arr[: n // 2]
+        right = arr[n // 2 :]
+        count = self.count(left, len(left), k) + self.count(right, len(right), k)
+        left_products = self.products(left, k)
+        right_products = self.products(right, k)
+        for p in left_products:
+            count += bisect(right_products, (k // p))  # TODO: fix
         return count
--- tests/test_subsets_product.py
+++ tests/test_subsets_product.py
@@ -2,7 +2,7 @@ from problems.subset_product import SubsetProduct
 
 
 class TestSubsetsProduct:
-    def test_numOfSubsets(self) -> None:
+    def test_count(self) -> None:
         assert SubsetProduct().count([], 0, 12) == 0
         assert SubsetProduct().count([5], 1, 12) == 1
         assert SubsetProduct().count([2, 5], 2, 12) == 3
