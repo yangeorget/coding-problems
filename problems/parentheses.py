80722bfd49a35a1756f0f79554066c2a5c0731ad
Yan Georget <yan@botfuel.io>
2022-03-24 12:14:05 +0100

    Improve algo for parentheses problem

--- problems/parentheses.py
+++ problems/parentheses.py
@@ -6,17 +6,22 @@ class Parentheses:
     """
 
     def max_length(self, string):
-        starts = [-1]  # TODO remove
+        """
+        The idea is to maintain a list of parenthesis indices.
+        The first index corresponds to the last extra closing parenthesis,
+        while the next ones all correspond to opening parenthesis (not closed yet).
+        """
+        starts = []
         max_len = 0
-        for i in range(0, len(string)):
-            if string[i] == "(":
+        # without loss of generality and without modifying the result,
+        # we can add an extra closing parenthesis at the beginning of the string
+        for i, p in enumerate(")" + string):
+            if p == "(":
                 starts.append(i)
             else:
-                if len(starts) != 0:
-                    starts.pop()
-                if len(starts) != 0:
-                    cur_len = i - starts[-1]
-                    max_len = max(max_len, cur_len)
+                if len(starts) <= 1:
+                    starts = [i]
                 else:
-                    starts.append(i)
+                    starts.pop()
+                    max_len = max(max_len, i - starts[-1])
         return max_len
