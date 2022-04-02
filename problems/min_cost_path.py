b31b9388b405fe1a24039509ba0c736236d976e7
Yan Georget <yan@botfuel.io>
2022-04-02 17:01:21 +0200

    Misc fixes

--- problems/min_cost_path.py
+++ problems/min_cost_path.py
@@ -4,6 +4,10 @@ MAX_VALUE = 500 * 500 * 1000
 
 
 class MinCostPath:
+    """
+    See https://practice.geeksforgeeks.org/problems/minimum-cost-path3833/1
+    Does not pass all examples : too slow !!!
+    """
     def minimum_cost_path(self, grid):
         n = len(grid)
         # all the following arrays could be matrices but have been flattened instead
@@ -16,6 +20,7 @@ class MinCostPath:
             [v for v in range(0, n * n)],
             [v for v in range(0, n * n)],
         )
+        heap.size = 1
         while heap.size > 0:
             v = heap.pop()
             visited[v] = True
@@ -26,27 +31,43 @@ class MinCostPath:
                 and not visited[v + n]
                 and values[v] + flat_grid[v + n] < values[v + n]
             ):
+                should_add = values[v + n] == MAX_VALUE
                 values[v + n] = values[v] + flat_grid[v + n]
-                heap.up(heap.indices[v + n])
+                if should_add:
+                    heap.add(v + n)
+                else:
+                    heap.up(heap.indices[v + n])
             if (
                 (v % n) + 1 < n
                 and not visited[v + 1]
                 and values[v] + flat_grid[v + 1] < values[v + 1]
             ):
+                should_add = values[v + 1] == MAX_VALUE
                 values[v + 1] = values[v] + flat_grid[v + 1]
-                heap.up(heap.indices[v + 1])
+                if should_add:
+                    heap.add(v + 1)
+                else:
+                    heap.up(heap.indices[v + 1])
             if (
                 0 <= v - n
                 and not visited[v - n]
                 and values[v] + flat_grid[v - n] < values[v - n]
             ):
+                should_add = values[v - n] == MAX_VALUE
                 values[v - n] = values[v] + flat_grid[v - n]
-                heap.up(heap.indices[v - n])
+                if should_add:
+                    heap.add(v - n)
+                else:
+                    heap.up(heap.indices[v - n])
             if (
                 0 < (v % n)
                 and not visited[v - 1]
                 and values[v] + flat_grid[v - 1] < values[v - 1]
             ):
+                should_add = values[v - 1] == MAX_VALUE
                 values[v - 1] = values[v] + flat_grid[v - 1]
-                heap.up(heap.indices[v - 1])
+                if should_add:
+                    heap.add(v - 1)
+                else:
+                    heap.up(heap.indices[v - 1])
         return MAX_VALUE
